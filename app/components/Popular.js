4700f92f0fec5d751cda7150546a8182c57cd3a2
Sami Abz <sami.abderre@gmail.com>
2019-08-07 15:08:51 +0100

    Use fetchPopularRepos()  to fetch repos

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,23 +1,59 @@
 import React, { Component } from 'react';
+import { fetchPopularRepos } from '../utils/api';
 import LanguagesNav from './LanguagesNav';
+
 
 class Popular extends Component {
   constructor() {
     super()
     this.state = {
-      selectedLanguage: 'All'
+      selectedLanguage: 'All',
+      repos: null,
+      error: null
     }
     this.updateLanguage = this.updateLanguage.bind(this)
+    this.isLoading = this.isLoading.bind(this)
+  }
+  componentDidMount () {
+    this.updateLanguage(this.state.selectedLanguage)
   }
  updateLanguage (lang) {
-  this.setState({ selectedLanguage: lang})
+  this.setState({ 
+    selectedLanguage: lang,
+    error: null,
+    repos: null
+  })
+
+  fetchPopularRepos(lang) 
+    .then((repos) => this.setState({
+      repos,
+      error: null
+    }))
+    .catch(() => {
+      console.warn('Error fetching repos: ', error)
+      this.setState({
+        error: `There was an error fetching the repositories.`
+      })
+    })
   }
+    isLoading () {
+      return this.state.repos === null && this.state.error === null
+    }
   render() {
+    const { selectedLanguage, repos, error} = this.state;
+  
     return (
-      <LanguagesNav 
-        selected={this.state.selectedLanguage}
-        onUpdateLangage={this.updateLanguage}
-      />
+      <React.Fragment>
+        <LanguagesNav 
+          selected={selectedLanguage}
+          onUpdateLangage={this.updateLanguage}
+        />
+        {this.isLoading() && <p>LOADING...</p>}
+
+        {error && <p>{error}</p>}
+
+        {repos && <pre>{JSON.stringify(repos, null, 2)}</pre>}
+      </React.Fragment>
     )
   }
 }
