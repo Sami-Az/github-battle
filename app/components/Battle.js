4b05556d7045b09955171763ee2cf6b792fad3fe
Sami Abz <sami.abderre@gmail.com>
2019-08-09 17:37:27 +0100

    Use <PlayerPreview /> as <PlayerInput /> display's result

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,40 +1,76 @@
 import React, { Component } from 'react';
-import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa';
 import PlayerInput from './PlayerInput';
-
-const Instructions = () => {
-  return (
-    <div className="instructions-container">
-      <h1 className="center-text header-lg">
-        INSTRUCTIONS
-      </h1>
-      <ol className="container-sm grid center-text battle-instructions">
-        <li>
-          <h3 className="header-sm">Enter two Github users</h3>
-          <FaUserFriends className="bg-light" color="rgb(255, 191, 116)" size={140} />
-        </li>
-        <li>
-          <h3 className="header-sm">Battle</h3>
-          <FaFighterJet className="bg-light" color="#727272" size={140} />
-        </li>
-        <li>
-          <h3 className="header-sm">See the winners</h3>
-          <FaTrophy className="bg-light" color="rgb(255, 215, 0)" size={140} />
-        </li>
-      </ol>
-    </div>
-  )
-}
+import PlayerPreview from './PlayerPreview';
+import Instructions from './Instructions';
 
 class Battle extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      playerOne: null,
+      playerTwo: null
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+  }
+
+  handleSubmit (id, player) {
+    id === "playerOne"
+     ? this.setState({
+        playerOne: player
+      }) 
+    : this.setState({
+        playerTwo: player
+      })
+  } 
+
+  handleReset(id) {
+    id === "playerOne"
+     ? this.setState({
+        playerOne: null
+       })
+    :  this.setState({
+        playerTwo: null
+       })
+  }
+
   render() {
+    const { playerOne, playerTwo } = this.state;
+
     return (
       <React.Fragment>
         <Instructions />
-        <PlayerInput 
-          label='label'
-          onSubmit={(value) => console.log('value !', value)}
-        />
+
+        <div className="players-container">
+          <h1 className="center-text header-lg">Players</h1>
+          <div className="row space-around">
+            {playerOne === null 
+             ? <PlayerInput 
+                  label='Player One'
+                  onSubmit={(player) => this.handleSubmit('playerOne', player)}
+                />
+             : <PlayerPreview 
+                  username={playerOne}
+                  label='Player One'
+                  onReset={() =>this.handleReset('playerOne')}
+               />
+            }
+
+            {playerTwo === null 
+              ? <PlayerInput 
+                  label='Player Two'
+                  onSubmit={(player) => this.handleSubmit('playerTwo', player)}
+                />
+              : <PlayerPreview 
+                  username={playerTwo}
+                  label='Player Two'
+                  onReset={() => this.handleReset('playerTwo')}
+                />
+            }
+          </div>
+        </div>  
       </React.Fragment>
     )
   }
