d4287d7f2b45b63da7d2ffa6fef9c1a58fc512dd
Sami Abz <sami.abderre@gmail.com>
2019-08-16 16:30:37 +0100

    Add nav links

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { ThemeConsumer } from '../context/theme';
+
+const activeStyle = {
+  color: 'gold'
+}
 
 export default function Nav () {
   return (
     <ThemeConsumer>
       {({ theme, toggleTheme }) => (
         <nav className="row space-between">
+          <ul className="row nav">
+            <li>
+              <NavLink to="/" 
+              exact
+              activeStyle={activeStyle}className="nav-link">Popular</NavLink>
+              </li>
+            <li>
+              <NavLink to="/battle"
+              activeStyle={activeStyle}className="nav-link">Battle</NavLink>
+            </li>
+          </ul>
           <button
             style={{fontSize: '30px'}}
             className="btn-clear"
