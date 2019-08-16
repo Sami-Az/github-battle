173288c337aeaaaabcc2d8feb1ca3c4f4cf0d73e
Sami Abz <sami.abderre@gmail.com>
2019-08-16 15:55:18 +0100

    Implement Theming functionality using react Context

--- app/index.js
+++ app/index.js
@@ -3,14 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Popular from './components/Popular';
 import Battle from './components/Battle';
+import Nav from './components/Nav';
+import { ThemeProvider } from './context/theme';
 
 class App extends React.Component {
+  constructor(props) {
+    super (props);
+
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }))
+      }
+    }
+  }
   render() {
     return (
-      <div className="container">
-        <Battle />
-        <Popular />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className={this.state.theme}>
+          <div className="container">
+              <Nav />
+              <Battle />
+              <Popular />
+          </div>
+        </div>
+      </ThemeProvider>
     )
   }
 }
