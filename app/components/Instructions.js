5407c7dedb5e18a623d36ada7dc91ffc356d0429
Sami Abz <sami.abderre@gmail.com>
2019-08-20 15:18:31 +0100

    Refactor <Instructions /> using hooks

--- app/components/Instructions.js
+++ app/components/Instructions.js
@@ -1,32 +1,29 @@
 import React from 'react';
 import { FaUserFriends, FaFighterJet, FaTrophy } from 'react-icons/fa';
-import { ThemeConsumer } from '../context/theme';
+import ThemeConext from '../context/theme';
 
 const Instructions = () => {
+  const theme =React.useContext(ThemeConext);
   return (
-    <ThemeConsumer>
-      {({ theme }) => (
-        <div className="instructions-container">
-          <h1 className="center-text header-lg instructions">
-            INSTRUCTIONS
-          </h1>
-          <ol className="container-sm grid center-text battle-instructions">
-            <li>
-              <h3 className="header-sm">Enter two Github users</h3>
-              <FaUserFriends className={`bg-${theme}`} color="rgb(255, 191, 116)" size={140} />
-            </li>
-            <li>
-              <h3 className="header-sm">Battle</h3>
-              <FaFighterJet className={`bg-${theme}`} color="#727272" size={140} />
-            </li>
-            <li>
-              <h3 className="header-sm">See the winners</h3>
-              <FaTrophy className={`bg-${theme}`} color="rgb(255, 215, 0)" size={140} />
-            </li>
-          </ol>
-        </div>
-      )}
-    </ThemeConsumer>
+    <div className="instructions-container">
+      <h1 className="center-text header-lg instructions">
+        INSTRUCTIONS
+      </h1>
+      <ol className="container-sm grid center-text battle-instructions">
+        <li>
+          <h3 className="header-sm">Enter two Github users</h3>
+          <FaUserFriends className={`bg-${theme}`} color="rgb(255, 191, 116)" size={140} />
+        </li>
+        <li>
+          <h3 className="header-sm">Battle</h3>
+          <FaFighterJet className={`bg-${theme}`} color="#727272" size={140} />
+        </li>
+        <li>
+          <h3 className="header-sm">See the winners</h3>
+          <FaTrophy className={`bg-${theme}`} color="rgb(255, 215, 0)" size={140} />
+        </li>
+      </ol>
+    </div>
   )
 }
 
