730cea7def76965b30f91ab0620db672e35bc47b
Sami Abz <sami.abderre@gmail.com>
2019-08-20 15:05:44 +0100

    Refactor <Hover /> using hooks

--- app/components/Hover.js
+++ app/components/Hover.js
@@ -1,21 +1,17 @@
 import React from 'react';
 
-export default class Hover extends React.Component {
-  state = {
-    hovering: false
-  }
-    
-  onMouseOver = () => { this.setState({ hovering: true }) }
-  onMouseOut = () => { this.setState({ hovering: false }) }
+export default function Hover ({ children }) {
+  const [hovering, setHovering] = React.useState(false);
+ 
+  const onMouseOver = () => { setHovering(true) }
+  const onMouseOut = () => { setHovering(false) }
   
-  render () {
-    return (
-      <div 
-        onMouseOver={this.onMouseOver} 
-        onMouseOut={this.onMouseOut}
-      >
-        {this.props.children(this.state.hovering)}
-      </div>
-    )
-  }
-  }
+  return (
+    <div 
+      onMouseOver={onMouseOver} 
+      onMouseOut={onMouseOut}
+    >
+      {children(hovering)}
+    </div>
+  )
+}
